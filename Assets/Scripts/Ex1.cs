587070543c85d4a766687dca59fecb27855010a4
ariadnetaaa04 <ariadna.perez.0401@gmail.com>
2022-11-22 12:44:01 +0100

    ex 1 fet

--- Assets/Scripts/Ex1.cs
+++ Assets/Scripts/Ex1.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Ex1 : MonoBehaviour
 {
+    public string letter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        IsVowel(letter);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+   private bool IsVowel(string l)
+    {
+        if (l=="a"||l=="e"|| l == "i"|| l == "o"|| l == "u" )
+        {
+            Debug.Log($"The letter is a vowel");
+          return true;
+        }
+        Debug.Log($"The letter is not a vowel");
+        return false;
     }
 }
